f34e680bab68215cb301d4e0cb2b29a0478b7776
Tatsuto YAMAMOTO <laminne33569@approvers.dev>
2023-05-24 09:45:51 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Qb - equipment & book management tool\n(C) 2023 Tatsuto Yamamoto")
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Qb
+equipment & book management tool
+
+
